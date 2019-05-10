c85caa845a3bb898bd11727174af2b3c3fb69f3f
thesinglecloud <hanfeng@zdns.cn>
2019-05-10 06:10:05 +0000

    update nodemanager

--- vendor/github.com/zdnscloud/lvmd/nodemanager.go
+++ vendor/github.com/zdnscloud/lvmd/nodemanager.go
@@ -71,7 +71,9 @@ func (m *NodeManager) initNodes() {
 	}
 
 	for _, n := range nl.Items {
-		m.addNode(&n, false)
+		if isStorageNode(&n) {
+			m.addNode(&n, false)
+		}
 	}
 }
 
@@ -111,10 +113,13 @@ func (m *NodeManager) getNode(name string) *Node {
 }
 
 func (m *NodeManager) GetNodesHasFreeSize(size uint64) []string {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
 	var candidates []string
-	for _, node := range m.nodes {
-		if node.FreeSize > size {
-			candidates = append(candidates, node.Name)
+	for _, n := range m.nodes {
+		if n.FreeSize > size {
+			candidates = append(candidates, n.Name)
 		}
 	}
 	return candidates
@@ -134,50 +139,63 @@ func (m *NodeManager) Release(name string, size uint64) error {
 }
 
 func (m *NodeManager) OnCreate(e event.CreateEvent) (handler.Result, error) {
-	m.lock.Lock()
-	defer m.lock.Unlock()
-
-	m.addNode(e.Object.(*corev1.Node), true)
+	n := e.Object.(*corev1.Node)
+	if isStorageNode(n) {
+		m.lock.Lock()
+		m.addNode(n, true)
+		m.lock.Unlock()
+	}
 	return handler.Result{}, nil
 }
 
 func (m *NodeManager) addNode(n *corev1.Node, checkExists bool) {
-	v, ok := n.Labels[ZkeStorageLabel]
-	if ok && v == "true" {
-		addr := n.Annotations[ZkeInternalIPAnnKey]
-		if checkExists {
-			old := m.getNode(n.Name)
-			if old != nil {
-				log.Warnf("node %s add more than once", n.Name)
-				return
-			}
+	addr := n.Annotations[ZkeInternalIPAnnKey]
+	if checkExists {
+		old := m.getNode(n.Name)
+		if old != nil {
+			log.Warnf("node %s add more than once", n.Name)
+			return
 		}
+	}
 
-		size, freeSize, err := m.getSizeInVG(addr + ":" + lvmdPort)
-		if err == nil {
-			log.Debugf("add node %s with cap %v", n.Name, freeSize)
-			m.nodes = append(m.nodes, &Node{
-				Name:     n.Name,
-				Addr:     addr,
-				Size:     size,
-				FreeSize: freeSize,
-			})
-		}
+	size, freeSize, err := m.getSizeInVG(addr + ":" + lvmdPort)
+	if err == nil {
+		log.Debugf("add node %s with cap %v", n.Name, freeSize)
+		m.nodes = append(m.nodes, &Node{
+			Name:     n.Name,
+			Addr:     addr,
+			Size:     size,
+			FreeSize: freeSize,
+		})
 	}
 }
 
 func (m *NodeManager) OnUpdate(e event.UpdateEvent) (handler.Result, error) {
+	old := e.ObjectOld.(*corev1.Node)
+	newNode := e.ObjectNew.(*corev1.Node)
+	if isStorageNode(newNode) {
+		isOldReady := isNodeReady(old)
+		isNewReady := isNodeReady(newNode)
+		if isOldReady != isNewReady {
+			m.lock.Lock()
+			if isNewReady {
+				m.addNode(newNode, true)
+			} else {
+				m.deleteNode(newNode.Name)
+			}
+			m.lock.Unlock()
+		}
+	}
+
 	return handler.Result{}, nil
 }
 
 func (m *NodeManager) OnDelete(e event.DeleteEvent) (handler.Result, error) {
-	m.lock.Lock()
-	defer m.lock.Unlock()
-
-	node := e.Object.(*corev1.Node)
-	v, ok := node.Labels[ZkeStorageLabel]
-	if ok && v == "true" {
-		m.deleteNode(node.Name)
+	n := e.Object.(*corev1.Node)
+	if isStorageNode(n) {
+		m.lock.Lock()
+		m.deleteNode(n.Name)
+		m.lock.Unlock()
 	}
 	return handler.Result{}, nil
 }
@@ -204,4 +222,19 @@ func (m *NodeManager) getSizeInVG(addr string) (uint64, uint64, error) {
 	}
 	defer conn.Close()
 	return conn.GetSizeOfVG(context.TODO(), m.vgName)
+}
+
+func isNodeReady(node *corev1.Node) bool {
+	for _, cond := range node.Status.Conditions {
+		if cond.Type == corev1.NodeReady &&
+			cond.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
+
+func isStorageNode(n *corev1.Node) bool {
+	v, ok := n.Labels[ZkeStorageLabel]
+	return ok && v == "true"
 }
